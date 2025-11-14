9f50adef69aee1854fd4d4070856c83f439f3525
s0ra1n0 <148192457+S0ra1n0@users.noreply.github.com>
2025-11-14 15:55:19 +0700

    Create Millennium College database schema

---
+++ Assignment1/Millennium_college_EERD.sql
@@ -0,0 +1,45 @@
+-- SQL Server Management Studio 20 --
+
+--CREATE DATABASE Millennium_College
+USE Millennium_College
+
+DROP TABLE IF EXISTS Enrollment
+DROP TABLE IF EXISTS Class
+DROP TABLE IF EXISTS Student
+DROP TABLE IF EXISTS Course
+DROP TABLE IF EXISTS Instructor
+
+CREATE TABLE Student (
+	Student_ID		INT	 PRIMARY KEY,
+	Student_name	VARCHAR(100) NOT NULL,
+	Major			VARCHAR(2)   NOT NULL
+);
+
+CREATE TABLE Course (
+	Course_ID		INT   PRIMARY KEY,
+	Course_Name		VARCHAR(50)  UNIQUE NOT NULL
+);
+
+CREATE TABLE Instructor (
+	Instructor_ID   INT   PRIMARY KEY,
+	Instructor_name VARCHAR(100) NOT NULL,
+	Location		VARCHAR(3)   UNIQUE NOT NULL,
+);
+
+CREATE TABLE Class (
+	Class_ID		INT	  PRIMARY KEY,
+	Course_ID		INT FOREIGN KEY REFERENCES Course(Course_ID),
+	Instructor_ID	INT FOREIGN KEY REFERENCES Instructor(Instructor_ID),
+	Semester		VARCHAR(5) NOT NULL,
+	Academic_Year	INT NOT NULL
+);
+
+CREATE TABLE Enrollment (
+	Enrollment_ID   INT     PRIMARY KEY,
+	Student_ID		INT FOREIGN KEY REFERENCES Student(Student_ID),
+	Class_ID		INT FOREIGN KEY REFERENCES Class(Class_ID),
+	Semester		VARCHAR(5) NOT NULL,
+	Academic_Year   INT		NOT NULL,
+	CGrade			CHAR(1) NOT NULL,
+	IGrade			NUMERIC(4, 2) NOT NULL
+);
